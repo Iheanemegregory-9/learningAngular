ca0bd319fed7505fbb2d241420c135d4e20470dd
Chibuzor Gregory Iheaneme <iheanemegregory9@gmail.com>
2023-04-29 22:07:50 +0100

    updated prodcuts module

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductListComponent } from '../product-list/product-list.component';
+import { ProductComponent } from '../product/product.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductListComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    ProductComponent
+  ],
+
+  exports: [ProductListComponent]
 })
 export class ProductsModule { }
