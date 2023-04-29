fb7e1f39399f76e867827317101b011c0031b551
Chibuzor Gregory Iheaneme <iheanemegregory9@gmail.com>
2023-04-29 22:07:11 +0100

    updated product list

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
 
+  currentStyle = {
+    color: 'greenyellow',
+    border: '1px solid red',
+    fontSize: '40px',
+    width: '80px',
+    padding: '20px'
+  }
+
+  
+
 }
