fc365b432256bed795ee4d63fbb8741c24ba0281
Chibuzor Gregory Iheaneme <iheanemegregory9@gmail.com>
2023-05-03 22:09:13 +0100

    added structural directives(*ngIf)

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
+
+import { ProductDetailComponent } from '../product-detail/product-detail.component';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements AfterViewInit {
 
   selectedProduct = '';
 
-  onBuy() {
-    window.alert('You just bought ' + this.selectedProduct + '!');
+  onBuy(name:string) {
+    window.alert(`You just bought ${this.selectedProduct}!`);
+  }
+
+  @ViewChild(ProductDetailComponent) productDetail: ProductDetailComponent | undefined;
+
+  ngAfterViewInit(): void {
+    if (this.productDetail) {
+      console.log(this.productDetail.name);
+    }
   }
 
 
