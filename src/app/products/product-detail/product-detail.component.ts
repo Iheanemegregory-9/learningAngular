84b0de583fa6a2207487c324d1d7d3ad2803c369
Chibuzor Gregory Iheaneme <iheanemegregory9@gmail.com>
2023-05-04 22:19:55 +0100

    added 'ngSwitch'

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,17 +1,40 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.css'],
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit, OnChanges {
 
   @Input() name = '';
-  @Output() bought = new EventEmitter();
+  @Output() bought = new EventEmitter<string>();
 
   buy() {
-    this.bought.emit();
+    this.bought.emit(this.name);
   }
+
+  get productName(): string {
+    console.log(`Get ${this.name}`);
+    return this.name;
+  }
+
+  constructor() {
+    console.log(`Name is ${this.name} in the constructor`)
+  }
+
+  ngOnInit(): void {
+    console.log(`Name is ${this.name} in the ngOnInit`);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const product = changes['name'];
+
+    if (!product.isFirstChange()) {
+      const oldValue = product.previousValue;
+      const newValue = product.currentValue;
+      console.log(`Product changed from ${oldValue} to    ${newValue}`)
+    }
+}
 
 }
