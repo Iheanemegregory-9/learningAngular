942328d195b47db16187fb593fdfb495cb95d909
Chibuzor Gregory Iheaneme <iheanemegregory9@gmail.com>
2023-05-05 12:28:18 +0100

    edited HTML using pipe

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -2,33 +2,44 @@ import { Component, AfterViewInit, ViewChild } from '@angular/core';
 
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
 
+import { Product } from '../product';
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements AfterViewInit {
+  selectedProduct: Product | undefined;
 
-  selectedProduct = '';
+  today = new Date();
 
-  products = [
-    "Webcam",
-    "Microphone",
-    "Wireless keyboard"
+  products: Product[] = [
+    {
+      name: 'Webcam',
+      price: 200,
+    },
+    {
+      name: 'Microphone',
+      price: 100,
+    },
+    {
+      name: 'Wireless Keyboard',
+      price: 85,
+    },
   ];
 
-  onBuy(name:string) {
-    window.alert(`You just bought ${this.selectedProduct}!`);
+  onBuy(name: string) {
+    window.alert(`You just bought ${this.selectedProduct?.name}!`);
   }
 
-  @ViewChild(ProductDetailComponent) productDetail: ProductDetailComponent | undefined;
+  @ViewChild(ProductDetailComponent) productDetail:
+    | ProductDetailComponent
+    | undefined;
 
   ngAfterViewInit(): void {
     if (this.productDetail) {
-      console.log(this.productDetail.name);
+      console.log(this.productDetail.product);
     }
   }
-
-
-
 }
